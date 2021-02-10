2bcf12a418326475a68704953b85abe947a3e25f
Bohomolov <bogomolov9567@gmail.com>
2021-02-10 17:46:47 +0200

    log4j 100% done

---
+++ src/main/java/log4jhw/blogic/RandomValue.java
@@ -0,0 +1,38 @@
+package log4jhw.blogic;
+
+import log4jhw.exception.MyException;
+import org.apache.log4j.Logger;
+
+import java.util.Random;
+
+public class RandomValue {
+    private final Random random = new Random();
+    private final static Logger logger = Logger.getLogger(RandomValue.class);
+
+    public int getRandomValue() {
+        int randomValue = random.nextInt(11);
+        if (randomValue <= 5) {
+            try {
+                throw new MyException("Сгенерированное число – " + randomValue);
+            } catch (MyException e) {
+                logger.error(e);
+            }
+        } else {
+            logger.info("Приложение успешно запущено");
+        }
+        return randomValue;
+    }
+
+    public static void main(String[] args) {
+        RandomValue randomValue = new RandomValue();
+        randomValue.getRandomValue();
+        randomValue.getRandomValue();
+        randomValue.getRandomValue();
+        randomValue.getRandomValue();
+        randomValue.getRandomValue();
+        randomValue.getRandomValue();
+        randomValue.getRandomValue();
+        randomValue.getRandomValue();
+    }
+
+}
